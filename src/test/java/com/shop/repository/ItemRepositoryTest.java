627f1b423950ad5e86a5ba13a8f45db9f8392e1c
yjn33 <yjn0032@gmail.com>
2025-03-08 20:58:42 +0900

    test: ItemRepository 테스트 코드

---
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.shop.repository;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest(){
+
+        Item item = new Item();
+
+        item.setItemNm("테스트용 상품");
+        item.setPrice(5500);
+        item.setItemDetail("상품 상세 설명 입니다");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(55); // 상품 재고 수량
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+
+        Item savedItem = itemRepository.save(item);
+        System.out.println(savedItem.toString());
+
+    }
+
+}
